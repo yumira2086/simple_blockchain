661be717f60f6eff644673e74795c5699eb6d483
yumira2086 <yumira2086@gmail.com>
2018-08-06 09:56:16 +0800

    修改部分常量

--- src/main/java/com/blockchain/common/Constants.java
+++ src/main/java/com/blockchain/common/Constants.java
@@ -12,47 +12,10 @@ package com.blockchain.common;
 import java.math.BigInteger;
 
 /**
- * ClassName:Constants <br/>
- *
+ * 常量类
  */
 public interface Constants {
-	
-	int PUBKEY_DIGEST_LENGTH = 90; // public key length
-	int PRVKEY_DIGEST_LENGTH = 45; //private key length
-	int ADDR_DIGEST_LENGTH = 35;   // address length
-	int SIGN_DIGEST_LENGTH = 98;   // signature length
-	int KEY_DES3_DIGEST_LENGTH = 24;  // max size of key for DES3 encrypt
-	int KEY_AES128_DIGEST_LENGTH = 16; // max size of key for AES128 encrypt
-	int TRANSSQL_DIGEST_LENGTH = 8192; // max size of trans sql for TrustSQL
-	
-	String RANDOM_NUMBER_ALGORITHM = "SHA1PRNG";
-	String RANDOM_NUMBER_ALGORITHM_PROVIDER = "SUN";
-	BigInteger MAXPRIVATEKEY = new BigInteger("00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140", 16);
 
-	String INFO_SHARE_PUBKEY = "BC8s/4qEAvVl4Sv0LwQOWJcVU6Q5hBd+7LlJeEivVmUbdtwP4RTfN8x/G+muMhN8SrweyyVVMIcIrnMWoFqGfIA=";
-
-	/**
-	 * 最后一个区块hash的key，value就是最后一个区块的hash
-	 */
-	String KEY_LAST_BLOCK = "key_last_block";
-    /**
-     * 第一个区块hash的key，value就是第一个区块的hash
-     */
-    String KEY_FIRST_BLOCK = "key_first_block";
-	/**
-	 * 区块hash与区块本身的key value映射，key的前缀，如{key_block_xxxxxxx -> blockJson}
-	 */
-	String KEY_BLOCK_HASH_PREFIX = "key_block_";
-
-	String KEY_REQUEST_PREFIX = "key_request_";
-    /**
-     * 保存区块的hash和下一区块hash，key为hash，value为下一区块hash，方便获取某一个block的下一个Block
-     */
-	String KEY_BLOCK_NEXT_PREFIX = "key_next_";
-	/**
-	 * 每个表的权限存储key
-	 */
-	String KEY_PERMISSION = "key_permission_";
 	/**
 	 * 所有账户数据
 	 */
@@ -70,10 +33,6 @@ public interface Constants {
 	 */
 	String LOCAL_NODES = "local_nodes";
 	/**
-	 * 区块数据存储 hash 桶前缀
-	 */
-	String BLOCKS_BUCKET_PREFIX = "block_";
-	/**
 	 * 钱包数据存储 hash 桶前缀
 	 */
 	String WALLETS_BUCKET_PREFIX = "wallet_";
@@ -81,16 +40,6 @@ public interface Constants {
 	 * 本地节点公钥，也就是挖到矿转账的地址
 	 */
 	String PUBLICKEY = "publickey";
-	/**
-	 * 最后一个区块的区块高度
-	 */
-	String LAST_BLOCK_INDEX = BLOCKS_BUCKET_PREFIX+"last_block";
-
-	/**
-	 * 客户端节点列表存储 key
-	 */
-	String CLIENT_NODES_LIST_KEY = "client-node-list";
-
 	/**
 	 * 服务器分组名
 	 */
@@ -102,5 +51,9 @@ public interface Constants {
 	int TIMEOUT = 5000;
 
 	String CHARSET = "utf-8";
+
+	String RANDOM_NUMBER_ALGORITHM = "SHA1PRNG";
+	String RANDOM_NUMBER_ALGORITHM_PROVIDER = "SUN";
+	BigInteger MAXPRIVATEKEY = new BigInteger("00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140", 16);
 }
 
